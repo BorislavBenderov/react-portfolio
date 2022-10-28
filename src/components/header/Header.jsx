60c83e75aba9111c34fc43acff19a37d68a96a91
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-28 11:33:20 +0300

    Make Header component, install react icons and using them

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './header.css';
+import { CTA } from './CTA';
+import ME from '../../assets/me.jpg'
+import { HeaderSocials } from './HeaderSocials';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Borislav Benderov</h1>
+        <h5 className="text-light">Fullstack JS Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   );
 }
