b75dac73cd41066f039a07c51d7c89764f45aaf6
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-29 18:12:00 +0300

    Make contact component CSS and responsive design

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,19 +13,19 @@ export const Contact = () => {
       <div className='container contact__container'>
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>benderow94@gmail.com</h5>
             <a href="mailto:benderow94@gmail.com" target='_blank'>Send a message</a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>borislavbenderov</h5>
             <a href="https://m.me/borislav.benderov" target='_blank'>Send a message</a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
             <a href="" target='_blank'>Send a message</a>
