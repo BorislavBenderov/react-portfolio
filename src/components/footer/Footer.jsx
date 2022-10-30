409ae06d73c2cd6b297920e62e9dfe6394fa5cc0
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-30 07:51:27 +0200

    Make footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import './footer.css';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { IoLogoTwitter } from 'react-icons/io';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>EGATOR</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+
+        <div className="footer__socials">
+          <a href="htpps://facebook.com"><FaFacebookF /></a>
+          <a href="htpps://instagram.com"><FiInstagram /></a>
+          <a href="htpps://twitter.com"><IoLogoTwitter /></a>
+        </div>
+      </ul>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved</small>
+      </div>
+    </footer>
   );
 }
